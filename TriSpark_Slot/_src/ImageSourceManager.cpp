3d772802736b32cf09bd7604e0ca6bff7baefa16
sin spark <spaaaark.sin120@gmail.com>
2020-07-28 00:11:22 +0900

    [ADD]画像src読み込み(default, number)作成
    numberは途中まで。
    numberは変数の値を取得する関数を、関数ポインタとしてコンストラクタでクラスに記録させる。

--- TriSpark_Slot/_src/ImageSourceManager.cpp
+++ TriSpark_Slot/_src/ImageSourceManager.cpp
@@ -5,3 +5,53 @@ IImageSourceManager::IImageSourceManager() {
 	mCountX = 0; mCountY = 0;
 	mDirectionFlag = false;
 }
+
+bool IImageSourceManager::Init(StringArr pReadData) {
+	StrToNum(mImageID,			pReadData[1]);
+	StrToNum(mPosX,				pReadData[2]);
+	StrToNum(mPosY,				pReadData[3]);
+	StrToNum(mPosW,				pReadData[4]);
+	StrToNum(mPosH,				pReadData[5]);
+	StrToNum(mCountX,			pReadData[6]);
+	StrToNum(mCountY,			pReadData[7]);
+	StrToNum(mDirectionFlag,	pReadData[8]);
+	return true;
+}
+
+bool CImageSourceDefault::Init(StringArr pReadData) {
+	if (pReadData.size() <= 8) return false;
+	return IImageSourceManager::Init(pReadData);
+}
+
+unsigned int CImageSourceDefault::GetSourceIndexMax() {
+	return mCountX * mCountY;
+}
+
+SDrawImageSourceData CImageSourceDefault::GetImageSource(int pIndex) {
+	SDrawImageSourceData ans;
+	ans.x = 0; ans.y = 0; ans.w = 0; ans.h = 0;
+	ans.r = 0; ans.g = 0; ans.b = 0; ans.imageID = -1;
+	if (pIndex < 0 || pIndex >= GetSourceIndexMax()) return ans;
+
+	ans.r = 255; ans.g = 255; ans.b = 255;
+	ans.x = mPosX; ans.y = mPosY;
+	ans.w = mPosW; ans.h = mPosH;
+	ans.imageID = mImageID;
+	return ans;
+}
+
+CImageSourceNumber::CImageSourceNumber(int (* const pGetVarFunc)(std::string)) : mVariableGetter(pGetVarFunc) {
+	mDigit = 0; mAlign = 0;
+}
+
+bool CImageSourceNumber::Init(StringArr pReadData) {
+	if (pReadData.size() <= 11) return false;
+	if (!IImageSourceManager::Init(pReadData)) return false;
+
+	mNumValName = pReadData[9];
+	StrToNum(mDigit,	pReadData[10]);
+	StrToNum(mAlign,	pReadData[11]);
+
+	return true;
+}
+
--- TriSpark_Slot/_src/_header/ImageSourceManager.hpp
+++ TriSpark_Slot/_src/_header/ImageSourceManager.hpp
@@ -1,16 +1,47 @@
 #pragma once
 #include "SDrawImageSourceData.hpp"
 #include <string>
+#include <vector>
 
 class IImageSourceManager {
+protected:
+	int		mImageID;
 	int		mPosX, mPosY, mPosW, mPosH;
 	int		mCountX, mCountY;
 	bool	mDirectionFlag;
 
+	typedef std::vector<std::string> StringArr;
+
+	template<class T> void StrToNum(T& InputFor, std::string& p_Data) {
+		// [act]テンプレート型、1つ目の引数に2つ目の文字列から抽出した数字を代入します
+		//		bool型の場合は整数で判別し、0がfalse、それ以外がtrueとなる模様です
+		std::istringstream Data(p_Data);
+		Data >> InputFor;
+	};
+
 public:
 	IImageSourceManager();
 	typedef unsigned int UINT;
-	virtual bool					Init(std::string pReadData) = 0;
+	virtual bool					Init(StringArr pReadData);
 	virtual UINT					GetSourceIndexMax() = 0;
-	virtual SDrawImageSourceData	ImageSource(int pIndex) = 0;
+	virtual SDrawImageSourceData	GetImageSource(int pIndex) = 0;
+};
+
+class CImageSourceDefault : public IImageSourceManager {
+public:
+	CImageSourceDefault() {}
+	bool					Init(StringArr pReadData) override;
+	UINT					GetSourceIndexMax() override;
+	SDrawImageSourceData	GetImageSource(int pIndex) override;
+};
+
+class CImageSourceNumber : public IImageSourceManager {
+	int mDigit, mAlign;
+	std::string mNumValName;
+	int (* const mVariableGetter)(std::string);		// 変数値取得用関数ポインタ(int限定,引数はstring:変数名)
+public:
+	CImageSourceNumber(int (*const pGetVarFunc)(std::string));
+	bool					Init(StringArr pReadData) override;
+	UINT					GetSourceIndexMax() override;
+	SDrawImageSourceData	GetImageSource(int pIndex) override;
 };
