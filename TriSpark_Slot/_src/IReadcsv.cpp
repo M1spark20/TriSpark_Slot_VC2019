c8ace81800bcb8c5836b7373c5bc948c05a4bd17
sin spark <spaaaark.sin120@gmail.com>
2021-02-21 20:26:29 +0900

    [FIX]csvファイルの読み込みをDxLib依存に変更
    ifstreamをAndroid/iOS(おそらく)に使用できないため

--- TriSpark_Slot/_src/IReadcsv.cpp
+++ TriSpark_Slot/_src/IReadcsv.cpp
@@ -1,23 +1,27 @@
 ﻿#include <fstream>
 #include <iostream>
 #include "_header/IReadcsv.h"
+#include "DxLib.h"
 IReadCSVBase::IReadCSVBase(){
 	m_ReadDataNowCurrent = 0;
 }
 bool IReadCSVBase::StartReadFile(const char* FileName){
 // [act]メンバ変数m_ReadDataAllにファイルの文字列をすべて読み込む
 // [ret]ファイルが読み込めたかどうか
-	const int ReadLineStrMax = 16384;// 1行から読み込む最大の文字数
-	std::ifstream File(FileName);
-	if(!File){// ファイルが読み込めなかったとき
-		std::cout << u8"CSVファイル\"" << FileName <<u8"\"が読み込めませんでした。" << std::endl;
+	const int ReadLineStrMax = 8192;// 1行から読み込む最大の文字数
+	DxLib::SetUseASyncLoadFlag(FALSE);
+	const int fileHandle = FileRead_open(FileName);
+	if (fileHandle == 0) {
+		DxLib::ErrorLogFmtAdd(u8"CSVファイル\"%s\"が読み込めませんでした。", FileName);
 		return false;
 	}
-	while(!File.eof()){
-		m_ReadDataAll += (char)File.get();
+	while (DxLib::FileRead_eof(fileHandle) == 0) {
+		const char str = (char)DxLib::FileRead_getc(fileHandle);
+		m_ReadDataAll += str;
 	}
 	m_ReadDataAll += '\0';
-	return true;
+
+	return DxLib::FileRead_close(fileHandle) == 0;
 }
 void IReadCSVBase::GetStrSplitByComma(StringArr& StrArray){
 // [prm]p1;戻り値として扱う。各配列に左からコンマで区切られた文字列が入る。
